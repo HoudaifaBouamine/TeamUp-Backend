677bdb7e0c39ac361d37cf2cd1e237ab7c9280c7
houdaifa <houdaifa.bouamine@gmail.com>
2024-03-08 10:21:05 +0100

    create Models folder, assign this task to ishaq

---
+++ Models/Models.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// NOTE (ISHAK) : see the ER diagram and transfer it to models in this file, then seperate the models to different files inside Models folder
+
+namespace Models
+{
+    public class User
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+    
+        /// complite the rest
+    }
+
+    public class Project
+    {
+        // fill project
+    }
+
+    // add other classes
+}
